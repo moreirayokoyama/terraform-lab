f4a6c7c38c67477c67f690a9043561b80305eb55
Daniel Moreira Yokoyama <792153+moreirayokoyama@users.noreply.github.com>
2024-01-22 08:33:03 -0300

    Deploying EC2 instance

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  cloud {
+    organization = "example-org-927684"
+
+    workspaces {
+      name = "cli-workspace"
+    }
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
